d54bff50525931f99382b0ded3c8aa865d688bd7
julien <julien-9871@hotmail.fr>
2020-12-09 12:16:07 +0100

    [#3] delete avis in parking model

--- app/src/main/java/com/platine/firemap/data/api/model/Parking.java
+++ app/src/main/java/com/platine/firemap/data/api/model/Parking.java
@@ -1,4 +1,32 @@
 package com.platine.firemap.data.api.model;
 
+import java.util.List;
+
 public class Parking {
+    private int id;
+    private String name;
+    private int price;
+
+    public Parking(String name, int price, List<String> avis) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
 }
