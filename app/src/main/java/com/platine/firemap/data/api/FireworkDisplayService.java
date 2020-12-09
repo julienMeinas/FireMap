353014ed734cd1785a8f4d8dd4ad1f6a51331868
julien <julien-9871@hotmail.fr>
2020-12-09 12:32:50 +0100

    [#3] [code] add FireworkDisplayService for call jee api and get all fireworks

---
+++ app/src/main/java/com/platine/firemap/data/api/FireworkDisplayService.java
@@ -0,0 +1,24 @@
+package com.platine.firemap.data.api;
+
+import com.platine.firemap.data.api.model.FireworkResponse;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ *  Classes that call uses the news API
+ */
+public interface FireworkDisplayService {
+    /**
+     * Method to retrieve popular fireworks
+     *
+     * @return popular fireworks
+     */
+    @GET("https://firemap-api.herokuapp.com/firework-types/")
+    Single<FireworkResponse> getAllFireworks();
+}
