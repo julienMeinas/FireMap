4f83081e9a6db18a1ac8bae78a841d75e09b2aa3
julien <julien-9871@hotmail.fr>
2020-12-09 12:15:10 +0100

    [#3] create Fireworker model

--- app/src/main/java/com/platine/firemap/data/api/model/Fireworker.java
+++ app/src/main/java/com/platine/firemap/data/api/model/Fireworker.java
@@ -1,4 +1,37 @@
 package com.platine.firemap.data.api.model;
 
 public class Fireworker {
+    private int id;
+    private String name;
+    private float note;
+
+    public Fireworker(int id, String name, float note) {
+        this.id = id;
+        this.name = name;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getNote() {
+        return note;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNote(float note) {
+        this.note = note;
+    }
 }
