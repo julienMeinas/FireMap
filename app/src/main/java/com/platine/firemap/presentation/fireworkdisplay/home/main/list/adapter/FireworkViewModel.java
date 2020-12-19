4b55de3ddbf6eed064cd5f465310852ebaf08ab7
julien <julien-9871@hotmail.fr>
2020-12-19 16:49:13 +0100

    [Code] add attributes for fireworks

--- app/src/main/java/com/platine/firemap/presentation/fireworkdisplay/home/main/list/adapter/FireworkViewModel.java
+++ app/src/main/java/com/platine/firemap/presentation/fireworkdisplay/home/main/list/adapter/FireworkViewModel.java
@@ -1,8 +1,19 @@
 package com.platine.firemap.presentation.fireworkdisplay.home.main.list.adapter;
 
+import com.platine.firemap.data.api.model.Fireworker;
+import com.platine.firemap.data.api.model.Parking;
+
+import java.util.List;
+
 public class FireworkViewModel {
     private String address;
     private String date;
+    private int price;
+    private boolean handicAccess;
+    private int duration;
+    private String crowded;
+    private Fireworker fireworker;
+    private List<Parking> parkings;
 
     public String getAddress() {
         return address;
@@ -18,5 +29,53 @@ public class FireworkViewModel {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isHandicAccess() {
+        return handicAccess;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getCrowded() {
+        return crowded;
+    }
+
+    public Fireworker getFireworker() {
+        return fireworker;
+    }
+
+    public List<Parking> getParkings() {
+        return parkings;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setHandicAccess(boolean handicAccess) {
+        this.handicAccess = handicAccess;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setCrowded(String crowded) {
+        this.crowded = crowded;
+    }
+
+    public void setFireworker(Fireworker fireworker) {
+        this.fireworker = fireworker;
+    }
+
+    public void setParkings(List<Parking> parkings) {
+        this.parkings = parkings;
     }
 }
