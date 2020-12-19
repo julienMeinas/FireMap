7cae42b3bc8aea9b8ab5bc41fd5fb2ef7cc5e86c
julien <julien-9871@hotmail.fr>
2020-12-19 18:29:44 +0100

    [Code] add class for layout Info_activity

--- app/src/main/java/com/platine/firemap/presentation/fireworkdisplay/infoFirework/InfoFireworkActivity.java
+++ app/src/main/java/com/platine/firemap/presentation/fireworkdisplay/infoFirework/InfoFireworkActivity.java
@@ -1,4 +1,47 @@
 package com.platine.firemap.presentation.fireworkdisplay.infoFirework;
 
-public class InfoFireworkActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.platine.firemap.R;
+
+public class InfoFireworkActivity extends AppCompatActivity {
+    private static final String TAG = "InfoFireworkActivity";
+    private static final String PLACE_MESSAGE = "PLACE";
+    private static final String DATE_MESSAGE = "DATE";
+    private static final String PRICE_MESSAGE = "PRICE";
+    private static final String PARKING_MESSAGE = "PARKING";
+    private static final String ACCESS_HANDICAP_MESSAGE = "ACCESS_HANDICAP";
+    private static final String PEOPLE_MESSAGE = "PEOPLE";
+    private static final String FIREWORKER_MESSAGE = "FIREWOERKER";
+
+    private TextView place;
+    private TextView date;
+    private ImageView imagePrice;
+    private TextView price;
+    private ImageView imageParking;
+    private TextView parking;
+    private ImageView imageAccessHandicap;
+    private TextView accessHandicap;
+    private ImageView imagePeople;
+    private TextView people;
+    private TextView fireworker;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.artivity_info);
+
+        findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
