a8f03f5be574151e96d16cb312548146300f900e
julien <julien-9871@hotmail.fr>
2020-12-20 15:20:17 +0100

    [#5][Code] create class for edit service (empty)

--- app/src/main/java/com/platine/firemap/presentation/fireworkdisplay/editFirework/EditFireworkActivity.java
+++ app/src/main/java/com/platine/firemap/presentation/fireworkdisplay/editFirework/EditFireworkActivity.java
@@ -1,4 +1,26 @@
 package com.platine.firemap.presentation.fireworkdisplay.editFirework;
 
-public class EditFireworkActivity {
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.platine.firemap.R;
+
+public class EditFireworkActivity extends AppCompatActivity {
+    private static final String TAG = "EditFireworkActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        findViewById(R.id.button_back).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
 }
